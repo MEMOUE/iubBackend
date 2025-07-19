7bcff115560c15df32bd113421afb3224b773a16
MEMOUE <konememoue1998@gmail.com>
2025-07-19 16:38:04 +0000

    feat: init commit

---
+++ src/main/java/com/iubbakend/IubBakendApplication.java
@@ -0,0 +1,13 @@
+package com.iubbakend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IubBakendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IubBakendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iubbakend/IubBakendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iubbakend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IubBakendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
